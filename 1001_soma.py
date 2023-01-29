8ab3320e8f7c0201d04fd5b920ab85bed25c40a9
ANTONIOALGMAR <86206750+ANTONIOALGMAR@users.noreply.github.com>
2023-01-29 08:55:51 -0300

    Update 1001_soma.py

--- 1001_soma.py
+++ 1001_soma.py
@@ -1,7 +1,7 @@
 #Soma simples de dois numero com interacao do usuario
-A = int(input())
-B = int(input())
+A = int(input("Digite o primeiro numero:"))
+B = int(input("Digite o segundo numero:"))
 
 X = A+B
 
-print("X =",X)
+print("o valor da soma é :" X)
